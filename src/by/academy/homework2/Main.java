87e5c38cb2e742db35715c370e99a921845f97fa
koop9087 <77532145+koop9087@users.noreply.github.com>
2021-07-17 20:52:21 +0300

    Delete src/by/academy/homework2 directory

--- src/by/academy/homework2/Main.java
+++
@@ -1,9 +0,0 @@
-package by.academy.homework2;
-
-public class Main {
-    public static void main(String[] args) {
-        SaleInShop.calculateSale();
-        DataType.determinateDataType();
-        MultiplicatorTable.multiplication();
-    }
-}
